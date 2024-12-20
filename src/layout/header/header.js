57f44e61f9b963f8d9a67b804d7937d582b13936
Regina <rmyachina@mail.ru>
2024-12-20 13:32:14 +0300

    feat: header

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -3,17 +3,24 @@ import { isMobile } from 'globals/adaptive'
 void (function () {
     const header = document.querySelector('header')
     if (!header) return
-    console.log(isMobile)
 
     if (isMobile) {
-        const mobileWrapper = header.querySelector('.header__mobile-wrapper')
-        const menuButton = mobileWrapper.querySelector('.header__mobile-button')
-        mobileWrapper.addEventListener('click', (e) => {
-            if (e.target === menuButton) e.currentTarget.classList.toggle('opened')
-            if (mobileWrapper.classList.contains('opened')) {
-                menuButton.textContent = 'Закрыть меню'
-            } else {
+        const mobileHeader = document.querySelector('.header__mobile')
+        const menuButton = mobileHeader.querySelector('.header__menu-button')
+
+        const top = mobileHeader.offsetHeight
+        const headerMobileWrapper = mobileHeader.querySelector('.header__mobile-wrapper')
+        headerMobileWrapper.style.height = `calc(100svh - ${top}px)`
+        headerMobileWrapper.style.top = `${top}px`
+
+        menuButton.addEventListener('click', () => {
+            const menuOpened = mobileHeader.classList.contains('menu-opened')
+            if (menuOpened) {
+                mobileHeader.classList.remove('menu-opened')
                 menuButton.textContent = 'Меню'
+            } else {
+                mobileHeader.classList.add('menu-opened')
+                menuButton.textContent = 'Закрыть меню'
             }
         })
     }
